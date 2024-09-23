d4ade592658a6eafb2b9d836da66a772338f1b28
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-09-23 17:00:34 +0530

    initial project setup
    initial project setup with UserDefaults

--- PersistData-GU/ViewController.swift
+++ PersistData-GU/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUsrDefaults()
+    }
+    
+    
+    func setUsrDefaults() {
+        savetoUsrDefault(key: "User", value: "John Appleseed")
+        let str = readFromUsrDefault(key: "User")
+        nameLbl.text = String(str)
+        print(str)
+        navigationItem.title = str
     }
 
+    
+    //  persist data using UserDefaults:
+    func savetoUsrDefault(key: String, value: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(value, forKey: key)
+        
+    }
+    
+    
+    //  retrive persist data using UserDefaults:
+    func readFromUsrDefault(key: String) -> String {
+        
+        let defaults = UserDefaults.standard
+        guard let retrivedData = defaults.string(forKey: key) else { return "" }
+        return retrivedData
+        
+    }
+    
+    
 
 }
 
